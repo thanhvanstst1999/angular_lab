d9c52ed7c315f52ff4e1b7170b3924cf6db1594a
Unknown <thanhvanstst1999@gmail.com>
2018-12-18 09:22:13 +0700

    update

---
+++ source/js/app.js
@@ -0,0 +1,26 @@
+(function(){
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'this is the best product',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: 'this is the best product',
+            canPurchase: true,
+            soldOut: false
+        }
+    ];
+    
+})();
+
